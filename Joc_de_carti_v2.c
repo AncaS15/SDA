cba5d67b9c96c61e0a36f3dae8dc380787709cca
gravityboy29 <140201586+gravityboy29@users.noreply.github.com>
2024-11-08 17:39:39 +0200

    Update Joc_de_carti_v2.c

--- Joc_de_carti_v2.c
+++ Joc_de_carti_v2.c
@@ -10,7 +10,7 @@ typedef struct joc{
 joc_t *creare_nod(){
     joc_t *nod_nou=malloc(sizeof(joc_t));
     scanf("%s", nod_nou->nume);
-    scanf("%d", &nod_nou->carte);
+    nod_nou->carte=0;
     nod_nou->next=NULL;
     return nod_nou;
 }
@@ -19,15 +19,7 @@ joc_t *adaugare(joc_t *head){
     if(head==NULL)
         return creare_nod();
     
-    joc_t *nod_curent=head, *nod_nou;
-    // while(nod_curent!=NULL){
-    //     if(nod_curent->next==NULL){
-    //         nod_nou=creare_nod();
-    //         nod_curent->next=nod_nou;
-    //         return head;
-    //     }
-    //     nod_curent=nod_curent->next;
-    // }
+    joc_t *nod_curent=head;
     while(nod_curent->next!=NULL)
         nod_curent=nod_curent->next;
     nod_curent->next=creare_nod();
@@ -46,15 +38,42 @@ void afisare(joc_t *head){
     }
 }
 
+joc_t *eliminare(joc_t *head){
+    joc_t *nod_curent=head, *min=head, *prev_min=NULL, *prev=NULL;
+    while(nod_curent!=NULL){
+        if(nod_curent->carte<min->carte){
+            min=nod_curent;
+            prev_min=prev;
+        }
+        prev=nod_curent;
+        nod_curent=nod_curent->next;
+    }
+    if(min==head)
+        head=head->next;
+    else
+        prev_min->next=min->next;
+    free(min);
+    return head;
+}
+
 int main(){
     int nr_jucatori;
     joc_t *lista_joc=NULL;
     
     scanf("%d", &nr_jucatori);
-    for(int i=0;i<nr_jucatori;i++){
+    for(int i=0;i<nr_jucatori;i++)
         lista_joc=adaugare(lista_joc);
-    }
     afisare(lista_joc);
+    
+    while(lista_joc!=NULL){
+        joc_t *nod_curent=lista_joc;
+        while(nod_curent!=NULL){
+            scanf("%d", &nod_curent->carte);
+            nod_curent=nod_curent->next;
+        }
+        lista_joc=eliminare(lista_joc);
+        afisare(lista_joc);
+    }
     
     return 0;
 }
