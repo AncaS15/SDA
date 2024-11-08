864f5afba5766d5a14910a6418cef5357fe192a1
gravityboy29 <140201586+gravityboy29@users.noreply.github.com>
2024-11-08 15:31:58 +0200

    Update Joc_de_carti_v2.c

--- Joc_de_carti_v2.c
+++ Joc_de_carti_v2.c
@@ -16,24 +16,34 @@ joc_t *creare_nod(){
 }
 
 joc_t *adaugare(joc_t *head){
-    if(head==NULL){
-        head=creare_nod();
-        return head;
-    }
-    joc_t *nod_curent, *nod_nou;
-    nod_curent=head;
-    while(nod_curent!=NULL){
-        if(nod_curent->next==NULL){
-            nod_nou=creare_nod();
-            nod_curent->next=nod_nou;
-            return head;
-        }
+    if(head==NULL)
+        return creare_nod();
+    
+    joc_t *nod_curent=head, *nod_nou;
+    // while(nod_curent!=NULL){
+    //     if(nod_curent->next==NULL){
+    //         nod_nou=creare_nod();
+    //         nod_curent->next=nod_nou;
+    //         return head;
+    //     }
+    //     nod_curent=nod_curent->next;
+    // }
+    while(nod_curent->next!=NULL)
         nod_curent=nod_curent->next;
-    }
+    nod_curent->next=creare_nod();
+    return head;
 }
 
 void afisare(joc_t *head){
+    joc_t *nod_curent=head;
     
+    if(head==NULL)
+        return;
+    
+    while(nod_curent!=NULL){
+        printf("%s\n", nod_curent->nume);
+        nod_curent=nod_curent->next;
+    }
 }
 
 int main(){
@@ -44,7 +54,7 @@ int main(){
     for(int i=0;i<nr_jucatori;i++){
         lista_joc=adaugare(lista_joc);
     }
-        
+    afisare(lista_joc);
     
     return 0;
 }
