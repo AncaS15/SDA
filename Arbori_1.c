91bcfdc900b2249d4eb4031fb198768cd48decdf
gravityboy29 <140201586+gravityboy29@users.noreply.github.com>
2024-12-30 13:26:39 +0200

    Create Arbori_1.c

---
+++ Arbori_1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct arbore{
+    int x;
+    struct arbore *next_s;
+    struct arbore *next_d;
+} arb;
+
+arb *creare(int x){
+    arb *nod=malloc(sizeof(arb));
+    nod->x=x;
+    nod->next_s=NULL;
+    nod->next_d=NULL;
+    return nod;
+}
+
+arb *adaugare_nod(arb *head, int x){
+    if(head==NULL)
+        return creare(x);
+    if(head->x<x)
+        head->next_d=adaugare_nod(head->next_d,x);
+    if(head->x>x)
+        head->next_s=adaugare_nod(head->next_s,x);
+    return head;
+}
+
+arb *adaugare(arb *head, int x){
+    if(head==NULL)
+        return creare(x);
+    adaugare_nod(head,x);
+    return head;
+}
+
+void afisare(arb *head, int n){
+    if(head!=NULL){
+        afisare(head->next_d,n);
+        if(head->x>=n)
+            printf("%d\n", head->x);
+        afisare(head->next_s,n);
+    }
+}
+
+int main(){
+    char c[5];
+    int x,n;
+    arb *arb_t=NULL;
+    
+    scanf("%s", c);
+    while(strcmp(c,"stop")){
+        x=atoi(c);
+        arb_t=adaugare(arb_t,x);
+        scanf("%s", c);
+    }
+    scanf("%d", &n);
+    afisare(arb_t,n);
+    
+    return 0;
+}
