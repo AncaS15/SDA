5f5815a1893639dc1e92e19061f4a6697af355a7
Anca <140201586+AncaS15@users.noreply.github.com>
2026-03-18 18:22:39 +0200

    Update and rename Test_scurt_2_3.c to linked-list-remove-first-n-nodes.c

--- linked-list-remove-first-n-nodes.c
+++ linked-list-remove-first-n-nodes.c
@@ -17,15 +17,13 @@ int main()
     struct LISTA *prim = NULL;
     scanf("%d", &nr_noduri);
 
-    scanf("%d", &n); // valoarea arbitrara ce va fi transmisa functiei eliminare_nod(...) care va dicta numarul de noduri ce vor fi sterse de la inceputul listei.
+    scanf("%d", &n);
 
     for (i = 0; i < nr_noduri; i++)
     {
         prim = adaugare_nod(prim);
     }
-    // Inceput apelare functie eliminare_nod(...):
     prim=eliminare_nod(prim,n);
-    // Sfarsit apelare functie eliminare_nod(...):
     afisare_lista(prim);
     return 0;
 }
@@ -34,23 +32,21 @@ struct LISTA *adaugare_nod(struct LISTA *prim)
 {
     struct LISTA *curent = prim;
 
-    // creare nod nou:
     struct LISTA *nod_nou = (struct LISTA *)malloc(sizeof(struct LISTA));
-    scanf("%d", &nod_nou->data); // stocarea continutului pentru ultimul nod
-    nod_nou->nod_urmator = NULL; // legatura ultimului nod catre NULL
+    scanf("%d", &nod_nou->data); 
+    nod_nou->nod_urmator = NULL; 
 
-    // plasare pe ultimul nod:
     if (prim != NULL)
     {
         while (curent->nod_urmator != NULL)
         {
             curent = curent->nod_urmator;
         }
-        curent->nod_urmator = nod_nou; // ultimul nod din lista este legat la noul nod
+        curent->nod_urmator = nod_nou;
     }
     else
     {
-        prim = nod_nou; // lista este goala; noul nod devine primul nod din lista
+        prim = nod_nou;
     }
 
     return prim;
@@ -62,8 +58,6 @@ void afisare_lista(struct LISTA *prim){
         prim = prim->nod_urmator;
     }
 }
-
-////////////////////////////////////////////////////////////////////////////////
 
 struct LISTA *eliminare_nod(struct LISTA *prim, int n){
     struct LISTA *nod_curent;
