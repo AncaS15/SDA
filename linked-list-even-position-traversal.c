4203bbd3f53b0afff4df4530774555cb38ecb143
Anca <140201586+AncaS15@users.noreply.github.com>
2026-03-18 18:23:19 +0200

    Refactor linked list node addition and traversal

--- linked-list-even-position-traversal.c
+++ linked-list-even-position-traversal.c
@@ -20,9 +20,7 @@ int main()
     {
         prim = adaugare_nod(prim);
     }
-    // Inceput apelare functie parcurgere_noduri_pare(...):
     parcurgere_noduri_pare(prim);
-    // Sfarsit apelare functie parcurgere_noduri_pare(...):
     
     return 0;
 }
@@ -31,10 +29,9 @@ struct LISTA *adaugare_nod(struct LISTA *prim)
 {
     struct LISTA *curent = prim;
 
-    // creare nod nou:
     struct LISTA *nod_nou = (struct LISTA *)malloc(sizeof(struct LISTA));
-    scanf("%d", &nod_nou->data); // stocarea continutului pentru ultimul nod
-    nod_nou->nod_urmator = NULL; // legatura ultimului nod catre NULL
+    scanf("%d", &nod_nou->data); 
+    nod_nou->nod_urmator = NULL; 
 
     // plasare pe ultimul nod:
     if (prim != NULL)
@@ -43,17 +40,15 @@ struct LISTA *adaugare_nod(struct LISTA *prim)
         {
             curent = curent->nod_urmator;
         }
-        curent->nod_urmator = nod_nou; // ultimul nod din lista este legat la noul nod
+        curent->nod_urmator = nod_nou;
     }
     else
     {
-        prim = nod_nou; // lista este goala; noul nod devine primul nod din lista
+        prim = nod_nou; 
     }
 
     return prim;
 }
-
-////////////////////////////////////////////////////////////////////////////////
 
 void parcurgere_noduri_pare(struct LISTA *prim){
     struct LISTA *nod_curent=prim;
